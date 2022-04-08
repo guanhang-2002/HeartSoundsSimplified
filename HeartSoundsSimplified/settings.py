30c3e25cb5384fc85c1a43b20959a477ae2eff5e
guanhang-2002 <942199919@qq.com>
2022-04-08 19:53:58 +0800

    the latest one is wrong
    不该把user类放进去

--- HeartSoundsSimplified/settings.py
+++ HeartSoundsSimplified/settings.py
@@ -29,9 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'User',
-    # 使用自定义的用户类
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
