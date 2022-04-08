48ca2399ec494a215740c031832e3c5eacae5057
guanhang-2002 <942199919@qq.com>
2022-04-08 19:43:22 +0800

    debug for user error
    Migration admin.0001_initial is applied before its dependency User.0001_initial on database 'default'.

--- HeartSoundsSimplified/settings.py
+++ HeartSoundsSimplified/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'User',
-    'django.contrib.admin',
+    # 使用自定义的用户类
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
